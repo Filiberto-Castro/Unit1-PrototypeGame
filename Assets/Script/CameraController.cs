4dfef34fe1b9f1c1a777351af1aaa42d55ec237f
Filiberto_Castro <fcastrob@miumg.edu.gt>
2022-12-14 14:04:36 -0600

    agrego posision del offset de la camara

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(0, 5, -7);
 
     void Start()
     {
