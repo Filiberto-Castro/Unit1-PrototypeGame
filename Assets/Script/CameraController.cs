b8f9c1a87c2407550b4ac2ea402c6d47f1b46347
Filiberto_Castro <fcastrob@miumg.edu.gt>
2022-12-14 21:25:46 -0600

    agregando codigo para camara FPS

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // variables de la camara principal
     public GameObject player;
     private Vector3 offset = new Vector3(0, 5, -7);
 
+    // variables para el cambio de camara
+    private bool camaraActiva;
+    public GameObject cameraFPS;
+
     void Start()
     {
-        
+        cameraFPS.SetActive(false);
+        camaraActiva = true;
     }
 
     void LateUpdate()
     {
         // hace que la camara persiga al jugador
         transform.position = player.transform.position + offset;
+
+        // cambiar a camara de primera persona
+        if(Input.GetKeyDown(KeyCode.F) && camaraActiva)
+        {
+            cameraFPS.SetActive(true);
+            camaraActiva = false;
+        }else if(Input.GetKeyDown(KeyCode.F) && !camaraActiva) // cambiar a tercera persona
+        {
+            cameraFPS.SetActive(false);
+            camaraActiva = true;
+        }
     }
 }
